73cd5754197ea0f6a56753d4bd96869dcf2a2815
Luciano Baraúna <lucianobaraunadev@gmail.com>
2024-09-05 14:49:09 -0300

    modulo(03): finalizada Criando reducer do player

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
+import { player } from './slices/player'
 
 
 export const store = configureStore({
   reducer: {
-
+    player: player
   }
 })
 
