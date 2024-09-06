5b31a9c1a136a74c067d21c0aee10dee06346d58
Luciano Baraúna <lucianobaraunadev@gmail.com>
2024-09-06 19:34:28 -0300

    modulo(03): finalizada Criando hook global

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,16 +1,10 @@
 import ReactPlayer from 'react-player'
-import { useAppSelector } from '../store';
 import { useDispatch } from 'react-redux';
-import { next } from '../store/slices/player';
+import { next, useCurrentLesson } from '../store/slices/player';
 
 export function Video() {
   const dispatch = useDispatch();
-  const video = useAppSelector(state => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
-    const currentLesson =  state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-
-    return currentLesson
-  });
+  const {currentLesson} = useCurrentLesson();
 
   function handlePlayNext() {
     dispatch(next())
@@ -23,7 +17,7 @@ export function Video() {
         controls
         onEnded={handlePlayNext}
         playing
-        url={`https://www.youtube.com/watch?v=${video.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
     </div>
   );
