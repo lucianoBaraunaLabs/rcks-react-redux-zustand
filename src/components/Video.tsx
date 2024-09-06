fd910ddce0fc64b995a589b2c7d69a247d96c4de
Luciano Baraúna <lucianobaraunadev@gmail.com>
2024-09-06 19:23:52 -0300

    modulo(03): finalizada Configurando autoplay

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,19 +1,28 @@
 import ReactPlayer from 'react-player'
 import { useAppSelector } from '../store';
+import { useDispatch } from 'react-redux';
+import { next } from '../store/slices/player';
 
 export function Video() {
+  const dispatch = useDispatch();
   const video = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
     const currentLesson =  state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
 
     return currentLesson
   });
+
+  function handlePlayNext() {
+    dispatch(next())
+  }
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
+        onEnded={handlePlayNext}
+        playing
         url={`https://www.youtube.com/watch?v=${video.id}`}
       />
     </div>
