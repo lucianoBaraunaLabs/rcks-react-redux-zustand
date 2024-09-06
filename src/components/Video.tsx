1b072e43559e31ad3d33e261d3e09bc644501b6e
Luciano Baraúna <lucianobaraunadev@gmail.com>
2024-09-05 22:05:14 -0300

    modulo(03): finalizada Selecionando aula atual

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,13 +1,20 @@
 import ReactPlayer from 'react-player'
+import { useAppSelector } from '../store';
 
 export function Video() {
+  const video = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+    const currentLesson =  state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+
+    return currentLesson
+  });
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url="https://www.youtube.com/watch?v=Jai8w6K_GnY"
+        url={`https://www.youtube.com/watch?v=${video.id}`}
       />
     </div>
   );
