cfb8bd2a2859466544bef2e51d551b121819889c
Luciano Baraúna <lucianobaraunadev@gmail.com>
2024-09-09 22:42:41 -0300

    modulo(04): finalizada Carregando dados do curso

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,14 @@ import { useCurrentLesson } from "../store/slices/player";
 export function Header() {
   const {currentModule, currentLesson} = useCurrentLesson();
 
+  if(!currentLesson) {
+    return null
+  }
+
   return (
     <div className="flex flex-col gap-1">
       <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-      <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
+      <span className="text-sm text-zinc-400">Módulo "{currentModule?.title}"</span>
     </div>
   );
 }
--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -21,7 +21,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
     return { currentModuleIndex, currentLessonIndex };
   });
 
-  const lessons = useAppSelector(state => state.player.course.modules[moduleIndex].lessons);
+  const lessons = useAppSelector(state => state.player.course?.modules[moduleIndex].lessons);
 
   return (
     <Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
@@ -40,7 +40,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
+          {lessons && lessons.map((lesson, lessonIndex) => {
             const isCurrent = currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex;
 
             return (
